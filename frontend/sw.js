c725075b3659ad0da918c3e29bc6fcc0fcdd14f5
Iga <bucz1991@gmail.com>
2025-12-05 20:21:58 +0100

    feat: Utworzenie podstawowego szkieletu aplikacji

---
+++ frontend/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'exchange-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.js',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
