9d84310ba0337a5f600a8b1717085b0962caf0b5
Iga <bucz1991@gmail.com>
2025-12-06 14:04:09 +0100

    chore: Dodanie pliku prettiera i dodanie event listenera activate

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,24 +1,41 @@
+// Stałe aplikacji
 const VERSION = 'v1';
 const CACHE_NAME = `exchange-${VERSION}`;
+const APP_ASSETS = ['/', '/index.html', '/app.js', '/manifest.json', '/favicon.ico', '/assets'];
 
+// Instalacja Service Workera i cache'owanie zasobów aplikacji
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/app.js',
-                '/manifest.json',
-                '/favicon.ico'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_ASSETS);
+    })
+  );
+
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+// Aktywacja Service Workera i usuwanie starych cache'y
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-    );
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Obsługa żądań sieciowych i serwowanie zasobów z cache'a
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
