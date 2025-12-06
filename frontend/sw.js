b5026015335712a108cc209d0f615602a56f032b
Marcin <marcin_krolak@op.pl>
2025-12-06 15:02:44 +0100

    _lepsze czyszczenie cache'a

--- frontend/sw.js
+++ frontend/sw.js
@@ -27,11 +27,9 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
+        keys
+          .filter((key) => key.startsWith('exchange') && key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
       );
     })
   );
